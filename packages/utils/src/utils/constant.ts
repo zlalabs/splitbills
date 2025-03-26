74e9cbbccdf5b7a566ddfeda951bbc6d8ee3da97
dukerspace <montolsaklor@gmail.com>
2025-03-26 18:56:11 +0700

    feat(packages): add utils package

---
+++ packages/utils/src/utils/constant.ts
@@ -0,0 +1,16 @@
+export enum ERROR_MSG_TYPE {
+  SYSTEM = 'system'
+}
+
+export enum ADMIN_ROLE {
+  SUPER_ADMIN = 'SUPER_ADMIN',
+  ADMIN = 'ADMIN',
+  MODERATOR = 'MODERATOR'
+}
+
+export const USER_ROLE = {
+  OWNER: 'OWNER'
+}
+
+export const MSG_DELETE_SUCCESS = 'Delete data success'
+export const MSG_WORKSPACE_NOT_FOUND = 'Workspace not found'
